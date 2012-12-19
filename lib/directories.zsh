95048e1c3e25464665a5cc8be0740912d2334528
Michel Bouwmans <m.bouwmans@ep-games.eu>
2012-12-19 15:47:29 +0100

    Add rationalise dot to directories.zsh

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -2,3 +2,14 @@
 setopt auto_name_dirs
 setopt auto_pushd
 setopt pushd_ignore_dups
+
+#Rationalise dot
+rationalise-dot() {
+  if [[ $LBUFFER = *.. ]]; then
+    LBUFFER+=/..
+  else
+    LBUFFER+=.
+  fi
+}
+zle -N rationalise-dot
+bindkey . rationalise-dot
